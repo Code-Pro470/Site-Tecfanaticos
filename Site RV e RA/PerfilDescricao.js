6df474b7d5fa2c548547167bd033c954e95cb2fa
Victor Hugo Da Silva <70340221+VictorSilva15@users.noreply.github.com>
2020-12-10 11:51:45 -0300

    Add files via upload

--- Site RV e RA/PerfilDescricao.js
+++ Site RV e RA/PerfilDescricao.js
@@ -2,7 +2,7 @@ function descricao1(){
 
     let describe = document.getElementById("des1");
 
-    describe.innerHTML = "<h5>Olá meu nome é Victor, tenho 15 anos e sou aluno do curso de Desenvolvimento de Sistemas. Nesse projeto fiquei encarregado pela <strong> Programação </strong>. </h5>";
+    describe.innerHTML = "<h5>Olá meu nome é Victor, tenho 15 anos e sou aluno do curso de Desenvolvimento de Sistemas. Nesse projeto fiquei encarregado pelo <strong>Conteúdo e Programação</strong>. </h5>";
     describe.style.textAlign = 'initial';
     describe.style.marginRight = '10px';
     describe.style.borderRadius = '10px';
@@ -16,7 +16,7 @@ function descricao2(){
 
     let describe = document.getElementById("des2");
 
-    describe.innerHTML = "<h5>Olá meu nome é Leonardo, tenho 15 anos e sou aluno do curso de Desenvolvimento de Sistemas. Nesse projeto fiquei encarregado pelo <strong> Design </strong>. </h5>";
+    describe.innerHTML = "<h5>Olá meu nome é Leonardo, tenho 15 anos e sou aluno do curso de Desenvolvimento de Sistemas. Nesse projeto fiquei encarregado pelo <strong>Conteúdo e Design</strong>. </h5>";
     describe.style.textAlign = 'initial';
     describe.style.marginRight = '10px';
     describe.style.borderRadius = '10px';
@@ -27,26 +27,12 @@ function descricao2(){
 
 
 }
-function descricao3(){
 
-    let describe = document.getElementById("des3");
-
-    describe.innerHTML = "<h5>Olá meu nome é Matheus, tenho 15 anos e sou aluno do curso de Desenvolvimento de Sistemas. Nesse projeto fiquei encarregado pelo <strong> Conteúdo </strong>. </h5>";
-    describe.style.textAlign = 'initial';
-    describe.style.marginRight = '10px';
-    describe.style.borderRadius = '10px';
-    describe.style.padding = '15px'
-    describe.style.font = "normal 13pt Arial";
-    describe.style.background = " rgb(87, 153, 197)";
-    describe.style.color = " white";
-
-
-}
 function descricao4(){
 
     let describe = document.getElementById("des4");
 
-    describe.innerHTML = "<h5>Olá meu nome é Lucas, tenho 15 anos e sou aluno do curso de Desenvolvimento de Sistemas. Nesse projeto fiquei encarregado pela <strong> Programação </strong>. </h5>";
+    describe.innerHTML = "<h5>Olá meu nome é Lucas, tenho 15 anos e sou aluno do curso de Desenvolvimento de Sistemas. Nesse projeto fiquei encarregado pelo <strong>Conteúdo e Programação</strong>. </h5>";
     describe.style.textAlign = 'initial';
     describe.style.marginRight = '10px';
     describe.style.borderRadius = '10px';
@@ -56,17 +42,4 @@ function descricao4(){
     describe.style.color = " white";
 
 }
-function descricao5(){
 
-    let describe = document.getElementById("des5");
-
-    describe.innerHTML = "<h5>Olá meu nome é Vitor Aquino, tenho 15 anos e sou aluno do curso de Desenvolvimento de Sistemas. Nesse projeto fiquei encarregado pelo <strong> Conteúdo </strong>. </h5>";
-    describe.style.textAlign = 'initial';
-    describe.style.marginRight = '10px';
-    describe.style.borderRadius = '10px';
-    describe.style.padding = '15px'
-    describe.style.font = "normal 13pt Arial";
-    describe.style.background = "rgb(39, 152, 210)";
-    describe.style.color = " white";
-
-}
